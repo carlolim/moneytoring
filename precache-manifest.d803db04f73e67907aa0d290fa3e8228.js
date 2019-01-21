d6ba64b35248a415cc2343537d38e1587d38bf06
Carlo James Lim <carlo@sprout.ph>
2019-01-21 21:42:47 +0800

    Updates

---
+++ precache-manifest.d803db04f73e67907aa0d290fa3e8228.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9062f253c6b7c86d3828",
+    "url": "/moneytoring/static/js/runtime~main.9062f253.js"
+  },
+  {
+    "revision": "114bbe7069df7520f935",
+    "url": "/moneytoring/static/js/main.114bbe70.chunk.js"
+  },
+  {
+    "revision": "fe81cedef25f68d97d40",
+    "url": "/moneytoring/static/js/1.fe81cede.chunk.js"
+  },
+  {
+    "revision": "114bbe7069df7520f935",
+    "url": "/moneytoring/static/css/main.0a1440a2.chunk.css"
+  },
+  {
+    "revision": "4b24671ad0cbbd7f5d52ad7204ff5436",
+    "url": "/moneytoring/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2dab20758bd925fbaecf42e3876af9b2.js"
+  "/moneytoring/precache-manifest.d803db04f73e67907aa0d290fa3e8228.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/moneytoring/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
