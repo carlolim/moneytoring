fd67d126df9f59e485c2df99b9681a413d0f508e
Carlo James Lim <carlo@sprout.ph>
2019-01-21 21:31:54 +0800

    Updates

---
+++ precache-manifest.2dab20758bd925fbaecf42e3876af9b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "37f6bd1c86b0dc28117c",
+    "url": "/static/js/main.37f6bd1c.chunk.js"
+  },
+  {
+    "revision": "fe81cedef25f68d97d40",
+    "url": "/static/js/1.fe81cede.chunk.js"
+  },
+  {
+    "revision": "37f6bd1c86b0dc28117c",
+    "url": "/static/css/main.0a1440a2.chunk.css"
+  },
+  {
+    "revision": "ff36faaa1f5459f90cd5f56461ae3062",
+    "url": "/index.html"
+  }
+];
