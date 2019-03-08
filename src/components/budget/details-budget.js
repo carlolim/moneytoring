af8daec1816be193e36d474d9c0f6505dbf0dd5d
Carlo James Lim <carlo@sprout.ph>
2019-03-08 21:07:37 +0800

    updates

--- src/components/budget/details-budget.js
+++ src/components/budget/details-budget.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
 import { withStyles } from "@material-ui/core/styles";
 import PropTypes from 'prop-types';
-import { Grid } from "@material-ui/core";
+import { Grid, IconButton } from "@material-ui/core";
+import ArrowBackIos from '@material-ui/icons/ArrowBackIos';
+import ArrowForwardIos from '@material-ui/icons/ArrowForwardIos';
 import MyToolbar from "../common/my-toolbar";
 import { selectById } from "../../helpers";
 
 const styles = {
+    textCenter: {
+        textAlign: 'center'
+    }
 }
 
 class DetailsBudget extends Component {
@@ -14,21 +19,34 @@ class DetailsBudget extends Component {
         budget: undefined
     }
 
-    async componentDidMount () {
+    async componentDidMount() {
         var budget = await selectById("budget", Number(this.props.match.params.id));
-        this.setState({budget, title: budget.name});
+        this.setState({ budget, title: budget.name });
     }
 
-    render () {
+    render() {
         return (
             <>
-                <MyToolbar 
+                <MyToolbar
                     onBack={() => { this.props.history.goBack() }}
                     showBackButton={true}
                     title={this.state.title}
                     buttons={[]}
                 />
                 <Grid container spacing={0}>
+                    <Grid className={this.props.classes.textCenter} item xs={2}>
+                        <IconButton>
+                            <ArrowBackIos />
+                        </IconButton>
+                    </Grid>
+                    <Grid className={this.props.classes.textCenter} item xs={8}>
+                        gm niko
+                    </Grid>
+                    <Grid className={this.props.classes.textCenter} item xs={2}>
+                        <IconButton>
+                            <ArrowForwardIos />
+                        </IconButton>
+                    </Grid>
                 </Grid>
             </>
         )
