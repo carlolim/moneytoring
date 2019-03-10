1c583e9acfff48cdcc6fddfb481eca507ad5b704
carlo lim <carlojameslim1@gmail.com>
2019-03-11 03:35:39 +0800

    implemented canvass charts

--- src/components/budget/details-budget.js
+++ src/components/budget/details-budget.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
 import { withStyles } from "@material-ui/core/styles";
 import PropTypes from 'prop-types';
-import { Grid, IconButton } from "@material-ui/core";
+import { Grid, IconButton, Typography } from "@material-ui/core";
 import ArrowBackIos from '@material-ui/icons/ArrowBackIos';
 import ArrowForwardIos from '@material-ui/icons/ArrowForwardIos';
 import MyToolbar from "../common/my-toolbar";
 import { selectById } from "../../helpers";
+import CanvasJSReact from '../../canvassjs/canvasjs.react';
 
+const CanvasJS = CanvasJSReact.CanvasJS;
+const CanvasJSChart = CanvasJSReact.CanvasJSChart;
 const styles = {
     textCenter: {
         textAlign: 'center'
@@ -15,13 +18,48 @@ const styles = {
 
 class DetailsBudget extends Component {
     state = {
-        title: "Loading...",
-        budget: undefined
+        budget: {
+            "budgetId": 0,
+            "name": "Loading...",
+            "accountIds": [],
+            "categoryIds": [],
+            "amount": 0,
+            "repeat": 0,
+            "startDate": new Date(),
+            "noEndDate": true,
+            "endDate": null,
+            "isActive": true,
+            "ledger": [
+                {
+                    "startDate": new Date(),
+                    "endDate": new Date(),
+                    "spent": 0,
+                    "amount": 0
+                }
+            ],
+            "showInDashboard": true,
+        },
+        filterDate: '',
+        options: {
+            title: {
+                text: "Basic Column Chart in React"
+            },
+            data: [{
+                type: "column",
+                dataPoints: [
+                    { label: "Apple", y: 10 },
+                    { label: "Orange", y: 15 },
+                    { label: "Banana", y: 25 },
+                    { label: "Mango", y: 30 },
+                    { label: "Grape", y: 28 }
+                ]
+            }]
+        }
     }
 
     async componentDidMount() {
         var budget = await selectById("budget", Number(this.props.match.params.id));
-        this.setState({ budget, title: budget.name });
+        this.setState({ budget });
     }
 
     render() {
@@ -30,7 +68,7 @@ class DetailsBudget extends Component {
                 <MyToolbar
                     onBack={() => { this.props.history.goBack() }}
                     showBackButton={true}
-                    title={this.state.title}
+                    title={this.state.budget.name}
                     buttons={[]}
                 />
                 <Grid container spacing={0}>
@@ -40,7 +78,7 @@ class DetailsBudget extends Component {
                         </IconButton>
                     </Grid>
                     <Grid className={this.props.classes.textCenter} item xs={8}>
-                        gm niko
+                        <Typography variant="overline"></Typography>
                     </Grid>
                     <Grid className={this.props.classes.textCenter} item xs={2}>
                         <IconButton>
@@ -48,6 +86,7 @@ class DetailsBudget extends Component {
                         </IconButton>
                     </Grid>
                 </Grid>
+                <CanvasJSChart options={this.state.options}/* onRef = {ref => this.chart = ref} */ />
             </>
         )
     }
