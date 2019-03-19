01ecf188ebf4dbb40d41cbf612465eec21b00bf3
Carlo James Lim <carlo@sprout.ph>
2019-03-19 20:22:53 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/moneytoring/precache-manifest.91814751a1431efee68d80ac5254c93c.js"
+  "/moneytoring/precache-manifest.505384b29059571f511946ba2a6d0434.js"
 );
 
 workbox.clientsClaim();
